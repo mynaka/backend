f9cf785e86e71550d8d4b58ae14f133f0e90ac8b
Amos Chaceter Ervin Villanueva <avillanueva@up.edu.ph>
2021-01-10 15:17:33 +0800

    feat: added startDate and Limit

--- routes/todo/get-many.js
+++ routes/todo/get-many.js
@@ -2,28 +2,59 @@ const { getTodos } =  require('../../lib/get-todos');
 const { join } = require('path');
 
 /**
- * Gets many todos
+ * Gets many todos from the database
  * 
  * @param {} app 
  */
 exports.getMany = app => {
-
     /**
-     * This gets the todos from the db
+     * This gets the todos from the database
+     *
+     * @param {import('fastify').FastifyRequest} request
      */
     app.get('/todo', (request) => {
-        const encoding = 'utf8';
-        const filename = join(__dirname, '../../database.json');
-        const todos = getTodos(filename, encoding);
-        const data = [];
-
-        for (const todo of todos){
+      const { query } = request;
+      const { limit = 3, startDate } = query;
+      const encoding = 'utf8';
+      const filename = join(__dirname, '../../database.json');
+      const todos = getTodos(filename, encoding);
+      const data = [];
+  
+  
+      if (!startDate) {
+        // if there is no startDate, we should sort the todos in a
+        // descending order based on
+        // dateUpdated
+        todos.sort((prev, next) => next.dateUpdated - prev.dateUpdated);
+        // sorts the todos in an ascending order based on
+        // dateUpdated
+      } else {
+        todos.sort((prev, next) => prev.dateUpdated - next.dateUpdated);
+      }
+  
+  
+      for (const todo of todos) {
+        // if there is no startDate (which is default)
+        // or the todoUpdated is within the startDate range
+        // it should do inside
+        if (!startDate || startDate <= todo.dateUpdated) {
+          // if data.length is still below the specified limit
+          if (data.length < limit) {
             data.push(todo);
+          }
         }
-    
-        return{
-            success: true,
-            data
-        }; 
+      }
+  
+      // if we want to sort it in a descending order
+      // we should put next first and subtract it with
+      // the previous.
+      data.sort((prev, next) => next.dateUpdated - prev.dateUpdated);
+  
+      return {
+        success: true,
+        data
+  
+      };
     });
-}; 
+  
+}
