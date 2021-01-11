e797c73e345a5d0b85f51637feaf4bb4c3084056
Amos Chaceter Ervin Villanueva <avillanueva@up.edu.ph>
2021-01-11 18:34:30 +0800

    feat: added fastify-swagger init

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const fastify = require('fastify');
+const swagger = require('fastify-swagger');
+const { definitions } = require('./definitions');
 const { routes } = require('./routes');
 const { connect } = require('./db');
+const { name: title, description, version } = require('./package.json');
 
 /**
  * Server Init
@@ -11,6 +14,22 @@ const { connect } = require('./db');
 exports.build = async (opts = { logger: false, trustProxy: false }) => {
     //initialize server using fastify
     const app = fastify(opts);
+
+    app.register(swagger, {
+        routePrefix: '/docs',
+        exposeRoute: true,
+        swagger: {
+          info: {
+            title,
+            description,
+            version
+          },
+          schemes: ['http', 'https'],
+          consumes: ['application/json'],
+          produces: ['application/json'],
+          definitions
+        }
+    })
 
     await connect();
 
