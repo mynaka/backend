21eb4d497cef2c6bcf07f7cfa78090fa74a92087
Amos Chaceter Ervin Villanueva <avillanueva@up.edu.ph>
2021-01-13 13:47:14 +0800

    feat: add reg of jwt

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const fastify = require('fastify');
 const swagger = require('fastify-swagger');
 const sensible = require('fastify-sensible');
+const jwt = require('fastify-jwt');
+const { readFileSync } = require('fs');
 const { errorHandler } = require('./error-handler');
 const { definitions } = require('./definitions');
 const { routes } = require('./routes');
 const { connect } = require('./db');
 const { name: title, description, version } = require('./package.json');
+
+const audience = 'this-audience';
+const issuer = 'localhost';
 
 /**
  * Server Init
@@ -19,6 +24,23 @@ exports.build = async (opts = { logger: false, trustProxy: false }) => {
 
     app.register(sensible).after(() => {
       app.setErrorHandler(errorHandler);
+    });
+  
+    app.register(jwt, {
+      secret: {
+        private: readFileSync('./cert/keyfile', 'utf8'),
+        public: readFileSync('./cert/keyfile.key.pub', 'utf8')
+      },
+      sign: {
+        algorithm: 'RS256',
+        audience,
+        issuer,
+        expiresIn: '1h'
+      },
+      verify: {
+        audience,
+        issuer
+      }
     });
   
     app.register(swagger, {
